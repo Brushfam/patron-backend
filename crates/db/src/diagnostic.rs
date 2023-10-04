62f58dabf5986f5dfd54d04fe1387f34b2f12090
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2023-10-04 12:55:40 +0300

    updated docs

--- crates/db/src/diagnostic.rs
+++ crates/db/src/diagnostic.rs
@@ -5,13 +5,26 @@ use serde::Serialize;
 #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
 #[sea_orm(table_name = "diagnostics")]
 pub struct Model {
+    /// Unique diagnostic identifier.
     #[sea_orm(primary_key)]
     pub id: i64,
+
+    /// Related build session identifier.
     pub build_session_id: i64,
+
+    /// Related file identifier.
     pub file_id: i64,
+
+    /// Diagnostic level.
     pub level: Level,
+
+    /// Diagnostic start file position.
     pub start: i64,
+
+    /// Diagnostic end file position.
     pub end: i64,
+
+    /// Diagnostic message.
     pub message: String,
 }
 
@@ -19,8 +32,11 @@ pub struct Model {
 #[sea_orm(rs_type = "i16", db_type = "Integer")]
 #[serde(rename_all = "snake_case")]
 pub enum Level {
+    /// An error was found, which prevents any build attempts.
     #[sea_orm(num_value = 0)]
     Error,
+
+    /// A warning was found, which may prevent any build attempts.
     #[sea_orm(num_value = 1)]
     Warning,
 }
